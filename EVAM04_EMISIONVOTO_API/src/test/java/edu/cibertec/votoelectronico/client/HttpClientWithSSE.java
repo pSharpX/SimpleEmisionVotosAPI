929b4715ffc89288b82886776eb27c5776a2de51
pSharpX <guardian2093@gmail.com>
2020-04-04 01:59:15 -0500

    redefine httpclient for server-sent events resources interface

--- EVAM04_EMISIONVOTO_API/src/test/java/edu/cibertec/votoelectronico/client/HttpClientWithSSE.java
+++ EVAM04_EMISIONVOTO_API/src/test/java/edu/cibertec/votoelectronico/client/HttpClientWithSSE.java
@@ -3,8 +3,11 @@ package edu.cibertec.votoelectronico.client;
 import java.util.Map;
 import java.util.function.Consumer;
 
+import javax.ws.rs.sse.SseEventSource;
+
 public interface HttpClientWithSSE {
 
-	public <T> void listenOn(String path, Map<String, Object> header, Consumer<T> consumer, Class<T> responseType);
+	public <T> SseEventSource listenOn(String path, Map<String, Object> header, Consumer<T> consumer,
+			Class<T> responseType);
 
 }

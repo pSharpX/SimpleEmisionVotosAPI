b2bbab91f63692d5879bcd6bfd025b290283bf76
pSharpX <guardian2093@gmail.com>
2020-04-06 16:12:52 -0500

    mongo db queries

---
+++ mongodb/aggregation.js
@@ -0,0 +1,15 @@
+db.votos.aggregate([
+    {
+        $lookup: {
+            from: "grupoPoliticos",
+            localField: "grupoPolitico", // or author.$id
+            foreignField: "_id",
+            as: "grupoPolitico"
+        }
+    },
+    { $group: { _id: "$grupoPolitico.nombre", votantes: { $push: "$dni" }, cantidad: { $sum: 1 } } },
+    { $unwind: "$_id" },
+    { $project : { _id: 0, grupoPolitico: "$_id", votantes: 1, cantidad: 1} }
+]);
+    
+// { $addFields: { grupoPolitico : { $sum: "$books.copies" } } },
---
+++ mongodb/queries.js
@@ -0,0 +1,112 @@
+/** To search */
+db.grupoPoliticos.find();
+db.votos.find();
+
+/** To Drop collections **/
+db.grupoPoliticos.drop();
+db.votos.drop();
+
+db.votos.aggregate([
+    {"$group" : {_id:"$grupoPolitico", count:{$sum:1}}}
+]);
+   
+// {"$unwind": "$grupoPolitico"}, 
+// {"$match": {"grupoPolitico.nombre": "P1"}},
+db.votos.aggregate([
+    {"$group": {"_id": "$grupoPolitico", "votantes": {"$addToSet": "$dni"}, "cantidad": {"$sum": 1}}},
+    {"$project": {"votantes": 1, "cantidad": 1, "grupoPolitico": "$_id"}}
+]);
+    
+// this doesn't work
+db.votos.aggregate([
+    {
+        $lookup: {
+            from: "grupoPoliticos",
+            localField: "grupoPolitico", // or author.$id
+            foreignField: "_id",
+            as: "grupoPolitico"
+        }
+    }
+]);
+    
+// workaround stage 2
+db.votos.aggregate([
+    {
+        $addFields: {
+            "grupoPolitico1": {
+                $arrayElemAt: [{ $objectToArray: "$grupoPolitico" }, 1]
+            }
+        }
+    },
+    {
+        $addFields: {
+            "grupoPolitico2": "$grupoPolitico1.v"
+        }
+    },
+    {
+        $lookup: {
+            from: "grupoPoliticos",
+            localField: "grupoPolitico['$id']", // or author.$id
+            foreignField: "_id",
+            as: "grupoPolitico"
+        }
+    }
+]);
+    
+// workaround stage 2
+db.votos.aggregate([
+    {
+        $addFields: {
+            "grupoPolitico1": {
+                $arrayElemAt: [{ $objectToArray: "$grupoPolitico" }, 1]
+            }
+        }
+    },
+    {
+        $addFields: {
+            "grupoPolitico2": "$grupoPolitico1.v"
+        }
+    },
+    {
+        $lookup: {
+            from: "grupoPoliticos",
+            localField: "grupoPolitico2", // or author.$id
+            foreignField: "_id",
+            as: "grupoPolitico"
+        }
+    },
+    {
+        $addFields: {
+            "grupoPolitico": { $arrayElemAt: ["$grupoPolitico", 0] }
+        }
+    }
+]);
+    
+// final workaround
+db.votos.aggregate([
+    {
+        $addFields: {
+            "grupoPolitico": {
+                $arrayElemAt: [{ $objectToArray: "$grupoPolitico" }, 1]
+            }
+        }
+    },
+    {
+        $addFields: {
+            "grupoPolitico": "$grupoPolitico.v"
+        }
+    },
+    {
+        $lookup: {
+            from: "grupoPoliticos",
+            localField: "grupoPolitico", // or author.$id
+            foreignField: "_id",
+            as: "grupoPolitico"
+        }
+    },
+    {
+        $addFields: {
+            "grupoPolitico": { $arrayElemAt: ["$grupoPolitico", 0] }
+        }
+    }
+]);
